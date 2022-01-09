2d051f1dd07fcacab1bfdb1f4b86f2b16eeea7d6
jaeyeon <jae518@naver.com>
2022-01-09 22:23:35 +0900

    책 반납 기능 추가

--- demo/src/main/java/com/example/controller/BorrowController.java
+++ demo/src/main/java/com/example/controller/BorrowController.java
@@ -1,15 +1,12 @@
 package com.example.controller;
 
-import com.example.domain.Book;
 import com.example.domain.CustomUserDetails;
 import com.example.domain.Member;
-import com.example.service.BookService;
 import com.example.service.BorrowService;
 import com.example.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,23 +16,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class BorrowController {
 
     private final BorrowService borrowService;
+    private final MemberService memberService;
 
     @PostMapping("/borrow/{bookId}")
     public void borrow(@PathVariable("bookId") Long bookId, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        System.out.println("BorrowController.borrow");
-        System.out.println("customUserDetails = " + customUserDetails);
-
-//        Member member = memberService.findByEmail(customUserDetails.getUsername());
-        borrowService.save(bookId, 4l, true);
+        Member member = memberService.findByEmail(customUserDetails.getUsername());
+        borrowService.save(bookId, member.getId(), true);
     }
 
     @PostMapping("/borrowBack/{bookId}")
     public void borrowBack(@PathVariable("bookId") Long bookId, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
-
-        System.out.println("BorrowController.borrow");
-        System.out.println("customUserDetails = " + customUserDetails);
-
-//        Member member = memberService.findByEmail(customUserDetails.getUsername());
-        borrowService.save(bookId, 4l, false);
+        Member member = memberService.findByEmail(customUserDetails.getUsername());
+        borrowService.save(bookId, member.getId(), false);
     }
 }
